da7ca4248a5d40717db2a8cd3c721687df38ecee
Riyapatil-ra <23be0313@sipnaengg.ac.in>
2026-09-04 10:03:30 +0530

    Complete day1 till Identity operators

--- Day1.py
+++ Day1.py
@@ -418,6 +418,7 @@ num = 6
 x = "Fri" if num == 5 else "Sat" if num == 6 else "Sun" if num == 7 else "weekday"
 print(x)
 
+# Identity operators
 a=["apple","mango","cherry"]
 b=["apple","mango","cherry"]
 x=10
