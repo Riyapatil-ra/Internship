a680a47dbd260812f9e7075f0ed9f357994d46d1
Riyapatil-ra <23be0313@sipnaengg.ac.in>
2026-09-06 23:10:54 +0530

    Completed functions

--- Day2.py
+++ Day2.py
@@ -928,8 +928,403 @@ This is called scope.
 ''' Local Scope
 * A variable created inside a function belongs to the local scope of that function, and can only be used inside that function.
 '''
+# def demo():
+#     x=300
+#     print(x)
+# demo()
+# print(x)
+''' Function Inside Function'''
+# def demo():
+#     x=300
+#     def demo2():
+#         print(x)
+#     demo2()
+# demo()
 
-def demo():
-    x=300
-    print(x)
-demo()
+''' Global Scope
+* A variable created in the main body of the Python code is a global variable 
+and belongs to the global scope.
+* Global variables are available from within any scope, global and local.
+'''
+# x=300
+# def demo():
+#     print(x)
+# demo()
+# print(x)
+
+''' Naming Variables'''
+# if same variable name is used inside and outside the function, 
+# then inside the function it will be treated as local variable and outside it will be treated as global variable.
+# x=300
+# def demo():
+#     x=200
+#     print(x)
+# demo()
+# print(x)
+
+''' Global Keyword'''
+# def demo():
+#     global x
+#     x=200
+#     print(x)
+# demo()
+# print(x)
+
+# x=300
+# def demo():
+#     global x
+#     x=200
+#     print(x)
+# demo()
+# print(x)
+
+''' NON-Local Keyword
+* The nonlocal keyword is used to work with variables inside nested functions.
+* The nonlocal keyword makes the variable belong to the outer function.
+'''
+# def myfunc1():
+#   x = "Jane"
+#   def myfunc2():
+#     nonlocal x
+#     x = "hello"
+#   myfunc2()
+#   return x
+
+# print(myfunc1())
+
+''' The LEGB Rule
+* L: Local  - Inside the current function
+* E: Enclosing  - Inside enclosing functions (from inner to outer)
+* G: Global - At the top level of the module
+* B: Built-in - In Python's built-in namespace
+'''
+# x = "global"
+
+# def outer():
+#   x = "enclosing"
+#   def inner():
+#     x = "local"
+#     print("Inner:", x)
+#   inner()
+#   print("Outer:", x)
+
+# outer()
+# print("Global:", x)
+
+''' Python Decorators
+* A decorator is a function that takes another 
+function as input and returns a new function.
+'''
+
+''' Basic Decorator 
+* Define the decorator first, 
+then apply it with @decorator_name above the function.
+'''
+# def changecase(func):
+#   def myinner():
+#     return func().upper()
+#   return myinner
+
+# @changecase
+# def myfunction():
+#   return "Hello Sally"
+
+# print(myfunction())
+
+''' Multiple Decorators'''
+# def changecase(func):
+#   def myinner():
+#     return func().upper()
+#   return myinner
+
+# @changecase
+# def myfunction():
+#   return "Hello Sally"
+
+# @changecase
+# def otherfunction():
+#   return "I am speed!"
+
+# print(myfunction())
+# print(otherfunction())
+
+''' Arguments in the decorator
+* If you want to pass arguments to the decorator,
+* you need to add another layer of functions.
+'''
+# def changecase(func):
+#   def myinner(x):
+#     return func(x).upper()
+#   return myinner
+
+# @changecase
+# def myfunction(nam):
+#   return "Hello " + nam
+
+# print(myfunction("John"))
+
+''' *args and **kwargs in Decorators'''
+# def changecase(func):
+#   def myinner(*args, **kwargs):
+#     return func(*args, **kwargs).upper()
+#   return myinner
+
+# @changecase
+# def myfunction(nam):
+#   return "Hello " + nam
+
+# print(myfunction("John"))
+
+''' Decorators with Arguments'''
+# def changecase(n):
+#   def changecase(func):
+#     def myinner():
+#       if n == 1:
+#         a = func().lower()
+#       else:
+#         a = func().upper()
+#       return a
+#     return myinner
+#   return changecase
+
+# @changecase(1)
+# def myfunction():
+#   return "Hello Linus"
+
+# print(myfunction())
+
+''' Multiple Decorators'''
+# def changecase(func):
+#   def myinner():
+#     return func().upper()
+#   return myinner
+
+# def addgreeting(func):
+#   def myinner():
+#     return "Hello " + func() + " Have a good day!"
+#   return myinner
+
+# @changecase
+# @addgreeting
+# def myfunction():
+#   return "Tobias"
+
+# print(myfunction())
+
+''' Prserving Function Metadata 
+* Functions in Python has metadata that can be accessed using the
+ __name__ and __doc__ attributes
+'''
+
+# def myfunction():
+#   return "Have a great day!"
+
+# print(myfunction.__name__)
+
+# def changecase(func):
+#   def myinner():
+#     return func().upper()
+#   return myinner
+
+# @changecase
+# def myfunction():
+#   return "Have a great day!"
+
+# print(myfunction.__name__)
+
+''' Preserving Metadata with functools.wraps'''
+# import functools
+
+# def changecase(func):
+#   @functools.wraps(func)
+#   def myinner():
+#     return func().upper()
+#   return myinner
+
+# @changecase
+# def myfunction():
+#   return "Have a great day!"
+
+# print(myfunction.__name__)
+
+''' Python Lambda
+* Synatx = lambda arguments: expression
+'''
+# x=lambda a : a + 10
+# print(x(5))
+
+# x=lambda a,b : a * b
+# print(x(5,6))
+
+# x=lambda a,b,c : a + b + c
+# print(x(5,6,2))
+
+''' Why use Lambda Functions?
+* Lambda functions are used when you need a nameless function for a short period of time.
+* They are often used as arguments to higher-order functions, which take other functions as arguments.
+'''
+# def demo(n):
+#     return lambda a:a*n
+
+# result=demo(3)
+# print(result(11))
+
+# def demo(n):
+#     return lambda a:a*n
+
+# result=demo(3)
+# result2=demo(5)
+# print(result(11))
+# print(result2(30))
+
+''' Lambda Functions with map(),filter(), and sorted()'''
+''' map()'''
+# nums=[1,2,3,4,5]
+# result=list(map(lambda x:x*2,nums))
+# print(result)
+
+''' filter()'''
+# nums=[1,2,3,4,5,6,7,8]
+# odd=list(filter(lambda x:x%2!=0,nums))
+# print(*odd)
+
+''' sorted()'''
+# students = [("Emil", 25), ("Tobias", 22), ("Linus", 28)]
+# sorted_students = sorted(students, key=lambda x: x[1])
+# print(sorted_students)
+
+# words = ["apple", "pie", "banana", "cherry"]
+# sorted_words = sorted(words, key=lambda x: len(x))
+# print(sorted_words)
+
+''' Python Recusion'''
+# def factorial(n):
+#   # Base case
+#   if n == 0 or n == 1:
+#     return 1
+#   # Recursive case
+#   else:
+#     return n * factorial(n - 1)
+# print(factorial(5))
+
+# def fibonacci(n):
+#   if n <= 1:
+#     return n
+#   else:
+#     return fibonacci(n - 1) + fibonacci(n - 2)
+# print(fibonacci(7))
+
+# def sum_list(numbers):
+#   if len(numbers) == 0:
+#     return 0
+#   else:
+#     return numbers[0] + sum_list(numbers[1:])
+# my_list = [1, 2, 3, 4, 5]
+# print(sum_list(my_list))
+
+# def find_max(numbers):
+#   if len(numbers) == 1:
+#     return numbers[0]
+#   else:
+#     max_of_rest = find_max(numbers[1:])
+#     return numbers[0] if numbers[0] > max_of_rest else max_of_rest
+# my_list = [3, 7, 2, 9, 1]
+# print(find_max(my_list))
+
+# import sys
+# print(sys.getrecursionlimit())
+
+# import sys
+# sys.setrecursionlimit(2000)
+# print(sys.getrecursionlimit())
+
+''' Python Generators
+* Generators are functions that can pause and resume their execution.
+* Generators saves Memory
+'''
+# def my_generator():
+#   yield 1
+#   yield 2
+#   yield 3
+
+# for value in my_generator():
+#   print(value)
+
+# def count_up_to(n):
+#   count = 1
+#   while count <= n:
+#     yield count
+#     count += 1
+
+# for num in count_up_to(5):
+#   print(num)
+
+# def large_sequence(n):
+#   for i in range(n):
+#     yield i
+
+# # This doesn't create a million numbers in memory
+# gen = large_sequence(1000000)
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+
+''' Using next() with Generators'''
+# def simple_gen():
+#   yield "Emil"
+#   yield "Tobias"
+#   yield "Linus"
+
+# gen = simple_gen()
+# print(next(gen))
+# print(next(gen))
+# print(next(gen))
+
+''' Generator Expressions'''
+# list_comp = [x * x for x in range(5)]
+# print(list_comp)
+# # Generator expression - creates a generator
+# gen_exp = (x * x for x in range(5))
+# print(gen_exp)
+# print(list(gen_exp))
+
+# total = sum(x * x for x in range(10))
+# print(total)
+
+''' Fibonacci Sequence using Generators'''
+# def fibonacci():
+#   a, b = 0, 1
+#   while True:
+#     yield a
+#     a, b = b, a + b
+# # Get first 100 Fibonacci numbers
+# gen = fibonacci()
+# for _ in range(10):
+#   print(next(gen))
+
+''' send() method
+* The send() method allows you to send a value to the generator:
+'''
+# def echo_generator():
+#   while True:
+#     received = yield
+#     print("Received:", received)
+
+# gen = echo_generator()
+# next(gen) # Prime the generator
+# gen.send("Hello")
+# gen.send("World")
+
+''' close() method
+* The close() method stops the generator:'''
+# def my_gen():
+#   try:
+#     yield 1
+#     yield 2
+#     yield 3
+#   finally:
+#     print("Generator closed")
+
+# gen = my_gen()
+# print(next(gen))
+# gen.close()
