c12bf8b6c16ec84b4de64fdf771824bc2b5c1d0a
Riyapatil-ra <23be0313@sipnaengg.ac.in>
2026-09-07 16:06:52 +0530

    Complete Program to demonstrate Regex and String Processing

---
+++ Task6.py
@@ -0,0 +1,30 @@
+''' Write a Python program that accepts a paragraph from the user and uses Regular Expressions (re) to extract:
+* Email addresses
+* 10-digit phone numbers
+* URLs
+* Numbers
+'''
+
+import re
+pattern1=r"[a-zA-Z0-9]+@[a-zA-Z]+\.(?:com|edu|net|org)"
+pattern2=r"[6-9]\d{9}"
+pattern3=r"https?://(?:www\.)?[a-zA-Z0-9]+\.(?:com|edu|net|org)"
+pattern4=r"\d+"
+
+user=input("Enter a para: ")
+email=re.findall(pattern1,user)
+phone=re.findall(pattern2,user)
+url=re.findall(pattern3,user)
+numbers=re.findall(pattern4,user)
+
+for i in phone:
+    numbers.remove(i)
+
+print("\nEmail addressses:")
+print(* email,sep="\n")
+print("\n10-digit phone numbers:")    
+print(* phone,sep="\n")
+print("\nURLs:")
+print(* url,sep="\n")
+print("\nNumbers:")
+print(* numbers,sep="\n")
